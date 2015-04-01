87d665f4dc36ffe86e6fcda97e63e218e5facc69
Sabba Petri <sabbap@gmail.com>
2015-04-01 14:03:10 -0700

    A completely new style

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,8 +6,35 @@
   };
 
   var setupEventHandlers = function() {
-    
+    $('.nav .left-side-nav ul li:first-child').on('click', about);
+    $('.nav .left-side-nav ul li:nth-child(2)').on('click', timeline);
+    $('.more').on('click', displayInfo);
   };
+
+  var displayInfo = function(e) {
+    $(e.target).prev().toggleClass('show');
+  }
+
+  var about = function() {
+    $('.nav div:nth-child(2) .timeline-row').removeClass('show');
+    $('.nav div:nth-child(2) .timeline-row').addClass('hide');
+    $('.nav div:nth-child(2) .basic-info .basic-info-inner').removeClass('hide');
+    $('.nav div:nth-child(2) .basic-info .basic-info-inner').addClass('show');
+    $('.nav').css('min-height', '800px');
+    $('.left-side-nav ul li').removeClass('active');
+    $('.left-side-nav ul li:first-child').addClass('active');
+
+  }
+
+  var timeline = function() {
+    $('.nav div:nth-child(2) .basic-info .basic-info-inner').removeClass('show');
+    $('.nav div:nth-child(2) .basic-info .basic-info-inner').addClass('hide');
+    $('.nav div:nth-child(2) .timeline-row').removeClass('hide');
+    $('.nav div:nth-child(2) .timeline-row').addClass('show');
+    $('.nav').css('min-height', '1400px');
+    $('.left-side-nav ul li').removeClass('active');
+    $('.left-side-nav ul li:nth-child(2)').addClass('active');
+  }
 
 window.beginApp = beginApp;
 
